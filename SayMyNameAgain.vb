f08046e33e916c1cf5dd86e9dc6e557ee9336f6e
unknown <dalekrebel@gmail.com>
2024-01-22 08:16:47 -0700

    Added Files

---
+++ SayMyNameAgain.vb
@@ -0,0 +1,25 @@
+﻿'Owen Fujii
+'RCET 2265
+'Spring 2024
+'SayYourName
+'
+Option Explicit On
+Option Strict On
+Module SayMyNameAgain
+
+
+    Sub Main()
+
+        Dim userName As String
+        'Ask user for name
+        Console.Write("What is your name?")
+        Console.WriteLine()
+        'Take user input
+        userName = Console.ReadLine()
+        'Give user output
+        Console.Write($"Pleasure to meet you {userName}")
+
+        Console.Read()
+    End Sub
+
+End Module
